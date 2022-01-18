3df2abd9825c7d75f54601ca6472ac5ee37e5513
Pozinux <tim.poznanski@gmail.com>
2022-01-18 12:00:30 +0100

    Update

--- portfolio_src/blog_app/views.py
+++ portfolio_src/blog_app/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 
 def all_blogs(request):
-    return render(request, 'blog_app/all_blogs.html')
+    return render(request, 'blog_app_templates/all_blogs.html')
